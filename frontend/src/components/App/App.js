1636bc66c3af5de70e00c8dcbeef98b27b7d3988
DNKO <denis.krainovic@outlook.at>
2020-05-11 23:13:38 +0200

    change homepage and add filepage

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,20 +1,43 @@
 import React from "react";
 import "./App.css";
-import MyDropzone from "../Dropzone/Dropzone";
+import Upload from "../Upload/Upload";
+import FilePage from "../FilePage/FilePage";
+import {
+   BrowserRouter as Router,
+   Switch,
+   Route,
+   Link
+} from "react-router-dom";
 
 function App() {
    return (
-      <div className="App">
-         <header className="header">
-            <div className="title-container">
-               <p className="title">Go File</p>
-            </div>
-            <div className="verticalLine" />
-            <div className="header-image" />
-         </header>
-         <MyDropzone />
-      </div>
+      <Router>
+         <div className="App">
+            <header className="header">
+               <div className="title-container">
+                  <p className="title">Go File</p>
+               </div>
+               <div className="verticalLine" />
+               <div className="header-image" />
+            </header>
+            <Switch>
+               <Route path="/uploads/:uploadID" component={FilePage} />
+               <Route path="/">
+
+                  <div className="MainBody">
+                     <div className="Card">
+                        <Upload />
+                     </div>
+                  </div>
+
+               </Route>
+            </Switch>
+         </div>
+      </Router>
+
    );
 }
+
+
 
 export default App;
--- frontend/src/components/Dropzone/Dropzone.jsx
+++ frontend/src/components/Dropzone/Dropzone.jsx
@@ -1,42 +1,88 @@
-import React, { useState } from "react";
-import Dropzone from "react-dropzone";
+import React, { Component } from "react";
 import "./Dropzone.css";
 
-const MyDropzone = () => {
-   const [file, setFile] = useState({});
-   const [fileSelected, setFileSelected] = useState(false);
+class Dropzone extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hightlight: false };
+    this.fileInputRef = React.createRef();
 
-   const uploadHandler = () => {
-      if (fileSelected) console.log(file);
-   };
+    this.openFileDialog = this.openFileDialog.bind(this);
+    this.onFilesAdded = this.onFilesAdded.bind(this);
+    this.onDragOver = this.onDragOver.bind(this);
+    this.onDragLeave = this.onDragLeave.bind(this);
+    this.onDrop = this.onDrop.bind(this);
+  }
 
-   return (
-      <React.Fragment>
-         <Dropzone
-            multiple={false}
-            onDrop={(acceptedFiles) => {
-               setFile(acceptedFiles);
-               setFileSelected(true);
-            }}
-         >
-            {({ getRootProps, getInputProps }) => (
-               <div {...getRootProps()} className="dropzone">
-                  <input {...getInputProps()} />
-                  <div>Drag and drop files or upload</div>
-               </div>
-            )}
-         </Dropzone>
-         {fileSelected ? <p>File: {`${file[0].name}`}</p> : null}
-         <button
-            className="upload-button"
-            type="button"
-            disabled={!fileSelected}
-            onClick={uploadHandler}
-         >
-            Generate Link
-         </button>
-      </React.Fragment>
-   );
-};
+  openFileDialog() {
+    if (this.props.disabled) return;
+    this.fileInputRef.current.click();
+  }
 
-export default MyDropzone;
+  onFilesAdded(evt) {
+    if (this.props.disabled) return;
+    const files = evt.target.files;
+    if (this.props.onFilesAdded) {
+      const array = this.fileListToArray(files);
+      this.props.onFilesAdded(array);
+    }
+  }
+
+  onDragOver(event) {
+    event.preventDefault();
+    if (this.props.disabed) return;
+    this.setState({ hightlight: true });
+  }
+
+  onDragLeave(event) {
+    this.setState({ hightlight: false });
+  }
+
+  onDrop(event) {
+    event.preventDefault();
+    if (this.props.disabed) return;
+    const files = event.dataTransfer.files;
+    if (this.props.onFilesAdded) {
+      const array = this.fileListToArray(files);
+      this.props.onFilesAdded(array);
+    }
+    this.setState({ hightlight: false });
+  }
+
+  fileListToArray(list) {
+    const array = [];
+    for (var i = 0; i < list.length; i++) {
+      array.push(list.item(i));
+    }
+    return array;
+  }
+
+  render() {
+    return (
+      <div
+        className={`Dropzone ${this.state.hightlight ? "Highlight" : ""}`}
+        onDragOver={this.onDragOver}
+        onDragLeave={this.onDragLeave}
+        onDrop={this.onDrop}
+        onClick={this.openFileDialog}
+        style={{ cursor: this.props.disabled ? "default" : "pointer" }}
+      >
+        <input
+          ref={this.fileInputRef}
+          className="FileInput"
+          type="file"
+          multiple
+          onChange={this.onFilesAdded}
+        />
+        <img
+          alt="upload"
+          className="Icon"
+          src="baseline-cloud_upload-24px.svg"
+        />
+        <span>Upload Files</span>
+      </div>
+    );
+  }
+}
+
+export default Dropzone;
